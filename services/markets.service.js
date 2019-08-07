94a04b84e0d7ca36e9ea65a59e0e1f9b6ad1b98c
Alex Goley <ajgoley@gmail.com>
2019-08-06 22:56:06 -0400

    use request promise

--- services/markets.service.js
+++ services/markets.service.js
@@ -1,22 +1,22 @@
 const ALPHA_ADVANTAGE_API_KEY = process.env.ALPHA_ADVANTAGE_API_KEY;
-const request = require("request");
-
-// TODO: Create wrapper of request that is observable? Or use request lib that utilizes promises?
+const rp = require("request-promise");
 
 const MarketsService = {
   nasdaq: (req, res, next) => {
-    request(
+    rp(
       "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=NASDAQ:.IXIC&apikey=" +
         ALPHA_ADVANTAGE_API_KEY,
-      { json: true },
-      (err, res2, body) => {
-        if (err) {
-          return console.log(err);
-        }
-        res.send(body);
+      { json: true }
+    )
+      .then(data => {
+        res.send(data);
         return next();
-      }
-    );
+      })
+      .catch(error => {
+        console.log(error);
+        res.send(error);
+        return next();
+      });
   }
 };
 
