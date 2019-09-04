ce7a5cbe536578f40e4d9c97cf27f5d87c7549c3
Alex Goley <ajgoley@gmail.com>
2019-09-03 22:18:55 -0400

    day series endpoint

--- contollers/index.controller.js
+++ contollers/index.controller.js
@@ -1,9 +1,11 @@
 const MarketsController = require("./markets.controller");
 const QueryController = require("./query.controller");
+const SeriesController = require("./series.controller");
 
 function IndexController(server) {
   MarketsController.markets(server);
   QueryController.query(server);
+  SeriesController.series(server);
 }
 
 module.exports = IndexController;
